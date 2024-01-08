2bde3cec64435c9ec0584367a0b8d3be622878ec
Ryan Stauffer <88114694+drdotnet22@users.noreply.github.com>
2024-01-08 16:33:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using BarcodeGenerator;
+using BarcodeStandard;
+using SkiaSharp;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace BarcodeGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string barcodeData = "ST1050124";
+            var b = new Barcode();
+            b.IncludeLabel = true;
+            SkiaSharp.SKImage img = b.Encode(BarcodeStandard.Type.Code128, barcodeData);
+            var data = img.Encode(SKEncodedImageFormat.Png, 100);
+            using (var stream = File.OpenWrite(@"C:\Users\Jeff\Downloads\barcode.png"))
+            {
+                data.SaveTo(stream);
+            }
+        }
+    }
+}
